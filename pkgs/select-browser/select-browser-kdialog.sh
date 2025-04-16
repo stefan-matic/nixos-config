561b77a43be5fda9482b9f13c0c4d1f2fa68232a
Stefan Matic <stefanmatic94@gmail.com>
2025-04-16 22:03:01 +0200

    Add right options for select-browser

--- pkgs/select-browser/select-browser-kdialog.sh
+++ pkgs/select-browser/select-browser-kdialog.sh
@@ -6,45 +6,45 @@ if [ "${APP}" = "" ]; then
     read -r -p " Do you want to install select-browser [Y/n] " RESPONSE
     case "${RESPONSE}" in
         [yY][eE][sS]|[yY]|"" )
-        echo "Installing..."
-        SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-        echo "${SCRIPTPATH}"
-        sudo cp "${SCRIPTPATH}"/select-browser-kdialog.sh /usr/local/bin/select-browser
-     ;;
+            echo "Installing..."
+            SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+            echo "${SCRIPTPATH}"
+            sudo cp "${SCRIPTPATH}"/select-browser-kdialog.sh /usr/local/bin/select-browser
+        ;;
         [nN][oO]|[nN])
-         echo "ok :'("
-           ;;
+            echo "ok :'("
+        ;;
         *)
-     echo "Invalid input..."
-     exit 1
-     ;;
+            echo "Invalid input..."
+            exit 1
+        ;;
     esac
     exit
 fi
 
 BROWSER=$(kdialog --title "Select your browser" --radiolist "Choose a browser" \
     1 "Chrome - Main" off \
-    2 "LibreWolf - Main" off \
+    2 "Firefox - Trustsoft" off \
     3 "Chrome - Second profile" off \
-    4 "Firefox - Main" off )
+4 "Ne znam jos" off )
 
 
 case "${BROWSER}" in
     "1")
         BROWSER_CMD="google-chrome-stable %U"
-        ;;
+    ;;
     "2")
-        BROWSER_CMD="flatpak run io.gitlab.librewolf-community"
-        ;;
+        BROWSER_CMD="firefox"
+    ;;
     "3")
         BROWSER_CMD="google-chrome-stable --profile-directory=\"Trustsoft\" %U"
-        ;;
+    ;;
     "4")
         BROWSER_CMD="firefox"
-        ;;
+    ;;
     *)
         exit 1
-        ;;
+    ;;
 esac
 
 ${BROWSER_CMD} "${*}"
