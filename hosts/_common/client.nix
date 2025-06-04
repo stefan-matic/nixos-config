f448386b9353169b9c304b64903a682b794f8a64
Stefan Matic <stefanmatic94@gmail.com>
2025-06-04 15:11:33 +0200

    Remove OBS crap from client common

--- hosts/_common/client.nix
+++ hosts/_common/client.nix
@@ -68,16 +68,6 @@
     ydotool
   ];
 
-
-  # OBS VIRTUAL CAM
-  boot = {
-    extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
-    extraModprobeConfig = ''
-      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-    '';
-  };
-  security.polkit.enable = true;
-
   # Yubico Authenticator
   services.pcscd.enable = true;
   services.udev.packages = [ pkgs.yubikey-personalization ];
