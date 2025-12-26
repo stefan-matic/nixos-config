41a1cf7cc00bee29325bad31942be1792d794f61
Stefan Matic <stefanmatic94@gmail.com>
2025-12-26 14:18:36 +0100

    Add pubspec for nordvpn

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,5 @@
   select-browser = pkgs.callPackage ./select-browser {};
   #deej-serial-control = pkgs.callPackage ./deej-serial-control {};
   #deej-new = pkgs.callPackage ./deej-new {};
-  nordvpn = pkgs.callPackage ./nordvpn/package.nix {
-    libxml2_13 = pkgs.libxml2;  # Use current libxml2, may need adjustment if 2.14+ causes issues
-  };
+  nordvpn = pkgs.callPackage ./nordvpn/package.nix {};
 }
