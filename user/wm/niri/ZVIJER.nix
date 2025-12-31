71e8e5aacdb94afadbb090fb37fcf0807a8bc49a
Stefan Matic <stefanmatic94@gmail.com>
2025-12-31 14:35:19 +0100

    set up dolphin to respect kde themes in niri

--- user/wm/niri/ZVIJER.nix
+++ user/wm/niri/ZVIJER.nix
@@ -87,8 +87,7 @@
       XDG_CURRENT_DESKTOP "niri"
       QT_QPA_PLATFORM "wayland"
       ELECTRON_OZONE_PLATFORM_HINT "wayland"
-      QT_QPA_PLATFORMTHEME "gtk3"
-      QT_QPA_PLATFORMTHEME_QT6 "gtk3"
+      QT_QPA_PLATFORMTHEME "kde"
       NIXOS_OZONE_WL "1"
     }
 
