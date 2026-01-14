4b085dcc5a095b45ad87525bda6bb8ed38029823
Stefan Matic <stefanmatic94@gmail.com>
2026-01-14 18:56:02 +0100

    Add stream fix package

--- user/packages/gaming.nix
+++ user/packages/gaming.nix
@@ -4,6 +4,11 @@
   # Gaming and entertainment
 
   home.packages = with pkgs; [
+    # Steam launcher with DISPLAY fix for xwayland-satellite
+    # Niri sets DISPLAY=:0 but xwayland-satellite connects on :1
+    # Use 'steam-fix' to launch Steam with correct display
+    steam-fix
+
     # Game Launchers
     lutris
 
