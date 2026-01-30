4be3e295e59f9def5b4d0abbc6f8a0635a88ffa8
Stefan Matic <stefanmatic94@gmail.com>
2026-01-30 21:47:00 +0100

    Add new firefox profiles to select-browser

--- pkgs/select-browser/select-browser-kdialog.sh
+++ pkgs/select-browser/select-browser-kdialog.sh
@@ -24,10 +24,12 @@ fi
 
 BROWSER=$(kdialog --title "Select your browser" --radiolist "Choose a browser" \
     1 "Chrome - Main" off \
-    2 "Firefox - Trustsoft" off \
-    3 "Chrome - Trustsoft" off \
-    4 "Chrome - OpenVPN" off \
-    5 "Chrome - Kupoprodaja" off
+    2 "Chrome - Trustsoft" off \
+    3 "Chrome - OpenVPN" off \
+    4 "Chrome - Kupoprodaja" off \
+    5 "Firefox - Main" off \
+    6 "Firefox - Trustsoft" off \
+    7 "Firefox - OpenVPN" off
 )
 
 
@@ -36,16 +38,22 @@ case "${BROWSER}" in
         exec google-chrome-stable --profile-directory="Default" "$@"
     ;;
     "2")
-        exec firefox "$@"
-    ;;
-    "3")
         exec google-chrome-stable --profile-directory="Profile 1" "$@"
     ;;
-    "4")
+    "3")
         exec google-chrome-stable --profile-directory="Profile 2" "$@"
     ;;
-    "5")
+    "4")
         exec google-chrome-stable --profile-directory="Profile 3" "$@"
+    ;;
+    "5")
+        exec firefox -P Main "$@"
+    ;;
+    "6")
+        exec firefox -P Trustsoft "$@"
+    ;;
+    "7")
+        exec firefox -P OpenVPN "$@"
     ;;
     *)
         exit 1
