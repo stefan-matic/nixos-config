633a57da13cf0820a655eff54c148542b0fd61fe
Stefan Matic <stefanmatic94@gmail.com>
2026-02-15 01:26:13 +0100

    Switching to unstable to ensure my insanity

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Stefan's journey of NixOS adoption";
 
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager?ref=release-25.11";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     dms = {
