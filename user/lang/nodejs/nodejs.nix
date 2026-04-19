04da88dc4c51d4c433c9954fdca2d582bc03e0b0
Stefan Matic <stefanmatic94@gmail.com>
2026-04-20 00:05:35 +0200

    Add mechanism to install npm packages globally on nixos (yeah yeah i know it's not declarative)

--- user/lang/nodejs/nodejs.nix
+++ user/lang/nodejs/nodejs.nix
@@ -5,4 +5,9 @@
     bun
     nodejs
   ];
+
+  # Redirect `npm install -g` into the user's home (the default global prefix
+  # lives inside the read-only Nix store and can't be written to).
+  home.sessionVariables.NPM_CONFIG_PREFIX = "$HOME/.npm-global";
+  home.sessionPath = [ "$HOME/.npm-global/bin" ];
 }
